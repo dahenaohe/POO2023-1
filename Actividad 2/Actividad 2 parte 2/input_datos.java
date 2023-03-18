e42c9c423046d12542f09032f99e18ceb7122d05
David Henao <125745988+dahenaohe@users.noreply.github.com>
2023-03-18 16:21:43 -0500

    Delete Actividad 2/Actividad 2 parte 2 directory

--- Actividad 2/Actividad 2 parte 2/input_datos.java
+++
@@ -1,8 +0,0 @@
-package Actividad 2 parte 2;
-
-public class input_datos {
-    public static void main(String[] args){
-        double circulo_r, cuadrado_l, triangulo_b, triangulo_h, rectangulo_b, rectangulo_h;
-        
-    }
-}
